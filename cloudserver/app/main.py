5745dbdf881132f08d8738b26ce7b6340bb1cfb8
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-10-04 19:10:36 +0900

    Add docker setting for cloudserver

--- cloudserver/app/main.py
+++ cloudserver/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-from room import Room, RoomDatabase
+from app.room import Room, RoomDatabase
 
 room_database = RoomDatabase()
 
