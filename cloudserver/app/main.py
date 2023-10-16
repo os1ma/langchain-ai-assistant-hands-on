67b711b0ce232ea0551a923146ce8bfbccc5911d
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-10-16 21:27:14 +0900

    Add basic auth

--- cloudserver/app/main.py
+++ cloudserver/app/main.py
@@ -1,6 +1,8 @@
-from typing import Optional
+import secrets
+from typing import Annotated, Optional
 
-from fastapi import FastAPI, HTTPException
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 
 from app.room import Room, RoomDatabase
@@ -15,12 +17,38 @@ async def root():
     return {"status": "ok"}
 
 
-@app.get("/rooms")
+security = HTTPBasic()
+
+
+def basic_auth(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"testuser"
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"testpassword"
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return
+
+
+room_router = APIRouter(prefix="/rooms", dependencies=[Depends(basic_auth)])
+
+
+@room_router.get("")
 async def get_rooms() -> list[Room]:
     return room_database.get_rooms()
 
 
-@app.get("/rooms/{room_id}")
+@room_router.get("/{room_id}")
 async def get_room(room_id: str) -> Room:
     if not room_database.exist_room_id(room_id):
         raise HTTPException(status_code=404, detail="Room not found")
@@ -28,7 +56,7 @@ async def get_room(room_id: str) -> Room:
     return room_database.get_room(room_id)
 
 
-@app.post("/rooms/{room_id}/register")
+@room_router.post("/{room_id}/register")
 async def create_room(room_id: str) -> Room:
     return room_database.register(room_id)
 
@@ -38,7 +66,7 @@ class UpdateRoomRequestBody(BaseModel):
     is_fan_on: Optional[bool] = None
 
 
-@app.post("/rooms/{room_id}/update")
+@room_router.post("/{room_id}/update")
 async def update_room(room_id: str, body: UpdateRoomRequestBody) -> Room:
     if not room_database.exist_room_id(room_id):
         raise HTTPException(status_code=404, detail="Room not found")
@@ -46,9 +74,12 @@ async def update_room(room_id: str, body: UpdateRoomRequestBody) -> Room:
     return room_database.update_room(room_id, body.is_light_on, body.is_fan_on)
 
 
-@app.get("/rooms/{room_id}/poll")
+@room_router.get("/{room_id}/poll")
 async def poll_room(room_id: str) -> Room:
     if not room_database.exist_room_id(room_id):
         raise HTTPException(status_code=404, detail="Room not found")
 
     return await room_database.poll(room_id)
+
+
+app.include_router(room_router)
--- cloudserver/test.sh
+++ cloudserver/test.sh
@@ -5,48 +5,59 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-ROOM_ID="testroom"
-SERVER_HOST="localhost"
-SERVER_PORT="80"
+readonly ROOM_ID="testroom"
+readonly SERVER_HOST=${SERVER_HOST:-localhost}
+readonly SERVER_PORT=${SERVER_PORT:-80}
+readonly BASIC_AUTH_USER=${BASIC_AUTH_USER:-testuser}
+readonly BASIC_AUTH_PASSWORD=${BASIC_AUTH_PASSWORD:-testpassword}
 
 # GET /
 curl -f http://${SERVER_HOST}:${SERVER_PORT}
 echo
 
 # GET /rooms
-curl -f http://${SERVER_HOST}:${SERVER_PORT}/rooms
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  http://${SERVER_HOST}:${SERVER_PORT}/rooms
 echo
 
 # POST /rooms/{room_id}/register
-curl -f -X POST \
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  -X POST \
   -H 'Content-Type: application/json' \
+  -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
   http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}/register
 echo
 
 # GET /rooms
-curl -f http://${SERVER_HOST}:${SERVER_PORT}/rooms
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  http://${SERVER_HOST}:${SERVER_PORT}/rooms
 echo
 
 # POST /rooms/{room_id}/update
 # update is_light_on
-curl -f -X POST \
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  -X POST \
   -H 'Content-Type: application/json' \
+  -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
   http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}/update \
   -d '{"is_light_on": true}'
 echo
 
 # POST /rooms/{room_id}/update
 # update is_fan_on
-curl -f -X POST \
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  -X POST \
   -H 'Content-Type: application/json' \
   http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}/update \
   -d '{"is_fan_on": true}'
 echo
 
 # GET /rooms/{room_id}
-curl -f http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}
 echo
 
 # GET /rooms/{room_id}/poll
-curl -f http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}/poll
+curl -f -u ${BASIC_AUTH_USER}:${BASIC_AUTH_PASSWORD} \
+  http://${SERVER_HOST}:${SERVER_PORT}/rooms/${ROOM_ID}/poll
 echo
