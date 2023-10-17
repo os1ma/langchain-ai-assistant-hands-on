12169f406d298cde0862e46c717c2b7a77d0963b
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-10-17 12:52:32 +0900

    Update picow check.py

--- picow/check.py
+++ picow/check.py
@@ -1,3 +1,5 @@
+import time
+
 from machine import Pin
 
 LIGHT_PIN_NO = 18
@@ -10,3 +12,9 @@ fan = Pin(FAN_PIN_NO, Pin.OUT)
 led.on()
 light.on()
 fan.on()
+
+time.sleep(5)
+
+led.off()
+light.off()
+fan.off()
